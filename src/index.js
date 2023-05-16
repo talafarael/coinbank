03b7f0e839ef7f41adfa0212718df79b1626c245
talafarael <127351345+talafarael@users.noreply.github.com>
2023-05-16 23:54:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,23 +4,30 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Routes, Route, Link } from "react-router-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
+import reportWebVitals from "./reportWebVitals";
 import Main from "./screens/Main";
 import Shop from "./screens/Shop";
 import Profile from "./screens/Profile";
+import { Provider } from "react-redux";
+import{ createStore } from 'redux';
+import rootReducer from "./reducers";
 
+
+const store =createStore(rootReducer)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/main" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+   <React.StrictMode>
+     <BrowserRouter>
+       <Routes>
+         <Route path="/profile" element={<Profile />} />
+         <Route path="/shop" element={<Shop />} />
+         <Route path="/main" element={<Main />} />
+       </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
