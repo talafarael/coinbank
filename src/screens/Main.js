d403881afe3707622a9ecec7d55e20cdbd2f6ad8
talafarael <127351345+talafarael@users.noreply.github.com>
2023-06-26 22:38:51 +0300

    Update Main.js

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,13 +1,21 @@
 import React from "react";
 import  Layout  from "../componets/Layout";
 import './Main.css'
+import Logovector from './Vector 4.png'
 
 function Main() {
   return (
   <div>
+
+   <img className="logo" src={Logovector } alt="load"></img>
+     <div className="mainText">Заробляйте розумом</div>
+   <div className="mainTitle">BANK-COIN</div>
+
+   <button className="mainButton" >Вперед</button>
    <div className='circle5'/>
    <div className='circle6'/>
    <div className='circle7'/>
+   
   </div>
   )
 }
