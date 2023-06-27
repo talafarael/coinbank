d17467647552d96e102ebb01a2f467b6416d0f08
talafarael <127351345+talafarael@users.noreply.github.com>
2023-06-27 17:06:24 +0300

    Update Main.js

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,7 +1,8 @@
 import React from "react";
 import  Layout  from "../componets/Layout";
 import './Main.css'
-import Logovector from './Vector 4.png'
+import Logovector from './photo/Vector 4.png'
+import union from "./photo/Union.png"
 
 function Main() {
   return (
@@ -10,8 +11,9 @@ function Main() {
    <img className="logo" src={Logovector } alt="load"></img>
      <div className="mainText">Заробляйте розумом</div>
    <div className="mainTitle">BANK-COIN</div>
-
    <button className="mainButton" >Вперед</button>
+   <div className="mainProfileText">Увійти в акаунт</div>
+   <img className="mainProfile" src={union}  alt="load"></img>
    <div className='circle5'/>
    <div className='circle6'/>
    <div className='circle7'/>
