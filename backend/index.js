fee19f522ce952dac3ada019125c68fee95ef669
talafarael <127351345+talafarael@users.noreply.github.com>
2023-09-30 18:51:40 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
+const authRouter=require('./authrouter')
 const PORT = process.env.PORT||8000
- app.listen(PORT, () => {
+const mongoose = require('mongoose');
+
+app.start=async()=>{
+    try{
+    await mongoose.connect(process.env.MONGO)
+    app.use('/auth', authRouter);
+    app.listen(PORT, () => {
             console.log(PORT);
         });
+
+    }catch{
+        console.log(e);
+    }
+}
+
+
 start()
