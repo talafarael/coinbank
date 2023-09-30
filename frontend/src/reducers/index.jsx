cffc6e3731eb78622a61191f5ae0efa78b010524
talafarael <127351345+talafarael@users.noreply.github.com>
2023-09-30 18:52:09 +0300

    Create index.jsx

---
+++ frontend/src/reducers/index.jsx
@@ -0,0 +1,8 @@
+import { CombinedState, combineReducers } from "@reduxjs/toolkit";
+import UserInfo from "./info";
+
+const rootReducer= combineReducers({
+    UserInfo
+})
+
+export default rootReducer
